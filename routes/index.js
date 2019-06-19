ef56c09eea820acdc93535be6be1596579ded8a5
HasmikAtom <hasmikatomyan@gmail.com>
2019-06-19 13:08:48 +0400

    cleanup

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,8 @@
 */
 const home = require('./home');
 const health = require('./health');
-const subscribe = require('./subscribe');
 
 module.exports = function initializeRoutes(app) {
   app.use('/', home);
   app.use('/health/', health);
-  app.use('/api/v1/subscribe/', subscribe);
 };
